39038acb8521b86493676b361e5a565fec0d2960
Isaac Lam <isaaclam05@gmail.com>
2026-06-28 18:58:35 +0800

    Updated website

---
+++ js/app.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const faqItems = document.querySelectorAll(".faq-item");
+
+    faqItems.forEach(item => {
+
+        const question = item.querySelector(".faq-question");
+
+        question.addEventListener("click", () => {
+
+            faqItems.forEach(other => {
+
+                if (other !== item) {
+
+                    other.classList.remove("active");
+
+                }
+
+            });
+
+            item.classList.toggle("active");
+
+        });
+
+    });
+
+});
